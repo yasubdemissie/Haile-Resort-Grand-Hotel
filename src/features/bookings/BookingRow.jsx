e7eaf26d18811b18cc16fd61453fe002acddf4b4
yasubdemissie <yasdam777@gmail.com>
2024-09-05 07:54:17 +0300

    The booking page end isvery near

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -7,7 +7,7 @@ import Menus from "../../ui/Menus";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
-import { HiEye } from "react-icons/hi2";
+import { HiArrowDownOnSquare, HiEye } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 
 const Cabin = styled.div`
@@ -95,6 +95,12 @@ function BookingRow({
             onClick={() => navigate(`/booking/${bookingId}`)}
           >
             See Details
+          </Menus.Button>
+          <Menus.Button
+            icon={<HiArrowDownOnSquare />}
+            onClick={() => navigate(`/booking/${bookingId}`)}
+          >
+            Check In
           </Menus.Button>
         </Menus.List>
       </Menus>
